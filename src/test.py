0b41dcfd280579301d3b3ba1311cb604fa5e3eac
bird0401 <mhiro7777777@gmail.com>
2023-05-21 21:38:16 +0900

    [update] test loading pkl file

--- src/test.py
+++ src/test.py
@@ -7,7 +7,7 @@ import glob, traceback, time
 
 import logging, logging.config
 from yaml import safe_load
-with open("./conf/logging.yml") as f:
+with open("../conf/logging.yml") as f:
     cfg = safe_load(f)
 logging.config.dictConfig(cfg)
 logger = logging.getLogger("main")
@@ -15,15 +15,15 @@ logger = logging.getLogger("main")
 import pickle
 
 def load_sentences():
-    sentences_by_entity = pickle.load(open("sentences_by_entity.pkl", "rb"))
+    sentences_by_entity = pickle.load(open("../sentences_by_entity.pkl", "rb"))
     return sentences_by_entity
 
 def main():
     sentences_by_entity = load_sentences()
     print(len(sentences_by_entity))
     for i, (k, v) in enumerate(sentences_by_entity.items()):
-        print(v)
         print(k)
+        print(v)
         if i == 10: break
 
 
